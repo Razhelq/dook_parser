43d912bba9d436e3a1bead26369d28ee00b65536
Razhel <mateuszszpakowski@wp.pl>
2020-01-26 18:44:30 +0100

    application done

--- parser.py
+++ parser.py
@@ -1,40 +1,142 @@
 import argparse
 import re
+from datetime import datetime
 
 
-class Log:
-    def __init__(self, file=None, fromm=None, to=None):
+class ArgumentParser:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument('--fromm', '--from', required=False)
+        self.parser.add_argument('--to', required=False)
+        self.parser.add_argument('file')
+        self.from_arg = None
+        self.to_arg = None
+        self.file = None
+        self.time_window = {'from': None, 'to': None}
+
+    def check_arguments(self):
+        args = self.parser.parse_args()
+        self.from_arg = args.fromm
+        self.to_arg = args.to
+        self.file = args.file
+        if self.from_arg or self.to_arg:
+            self.validate_time()
+
+    def validate_time(self):
+        if self.from_arg:
+            self.parse_time_frame('from')
+        if self.to_arg:
+            self.parse_time_frame('to')
+
+    def parse_time_frame(self, from_to: str) -> None:
+        time_frame = {
+            'from': {
+                'day': 00,
+                'month': 00,
+                'year': 0000,
+                'hour': 00,
+                'minute': 00,
+                'second': 00
+            },
+            'to': {
+                'day': 00,
+                'month': 00,
+                'year': 0000,
+                'hour': 00,
+                'minute': 00,
+                'second': 00,
+            }
+        }
+        try:
+            # Splits from/to argument into date and time strings
+            if from_to == 'from':
+                date = self.from_arg.split('_')
+            elif from_to == 'to':
+                date = self.to_arg.split('_')
+            day, month, year = date[0].split('-')
+            time_frame[from_to]['day'] = day
+            time_frame[from_to]['month'] = month
+            time_frame[from_to]['year'] = year
+            try:
+                # check if the time part is provided
+                time = date[1].split('-')
+                for i in range(len(time)):
+                    if i == 0:
+                        time_frame[from_to]['hour'] = time[0]
+                    if i == 1:
+                        time_frame[from_to]['minute'] = time[1]
+                    if i == 2:
+                        time_frame[from_to]['second'] = time[2]
+            except IndexError:
+                # no time part argument provided
+                pass
+            # Parse dictionary values into datetime object
+            from_to_time = f"{time_frame[from_to]['day']} " \
+                           f"{time_frame[from_to]['month']} " \
+                           f"{time_frame[from_to]['year']} " \
+                           f"{time_frame[from_to]['hour']} " \
+                           f"{time_frame[from_to]['minute']} " \
+                           f"{time_frame[from_to]['second']}"
+            self.time_window[from_to] = datetime.strptime(from_to_time, '%d %m %Y %H %M %S')
+        except ValueError as e:
+            # If the from/to format doesn't match correct format, value remain None
+            print('Please provide correct from/to values (eg. 23-12-1999_23-11-55)')
+
+    def return_arguments(self):
+        return self.time_window['from'], self.time_window['to'], self.file
+
+
+class Logg:
+    def __init__(self, file=None, from_arg=None, to_arg=None):
         self.file = file
-        self.fromm = fromm
-        self.to = to
+        self.time_window = {
+            'from': from_arg,
+            'to': to_arg,
+            'first': None,  # first date value required to count the requests_per_sec
+            'last': None    # last date value required to count the requests_per_sec
+        }
         self.output = {
             'requests': 0,
-            'requests_per_sec': 0,
+            'requests_per_sec': '',
             'responses': {},
-            '2xx_size': [0, 0],
+            '2xx_size': [0, 0],  # [how many values, overall size]
             'avg_2xx_size': 0
         }
 
     def open_file(self):
-        with open(self.file, 'r') as log:
-            self.read_file(log)
+        with open(self.file, 'r') as logg:
+            self.read_file(logg)
 
-    def read_file(self, log):
-        for line in log.readlines():
-            if self.fromm or self.to:
-                if self.validate_time(line):
-                    self.look_for_responses(line)
-                    self.look_for_size(line)
-            else:
+    def read_file(self, logg) -> None:
+        for line in logg.readlines():
+            if self.check_if_date_in_range(line):
                 self.look_for_responses(line)
                 self.look_for_size(line)
-        self.count_avr_2xx_size()
+        self.count_avg_2xx_size()
+        self.count_requests_per_second()
 
-    def validate_time(self):
-        self.parse_time()
-
-    def parse_time(self):
-
+    def check_if_date_in_range(self, line: str) -> bool:
+        date_regex = re.compile(r'(?<=\[).{20}(?=\s[-+]\d)')
+        log_date = re.search(date_regex, line)
+        try:
+            log_date_datetime = datetime.strptime(log_date.group(0), '%d/%b/%Y:%H:%M:%S')
+            if not self.time_window['to']:
+                # if 'to' value is None, first log entry is assigned (first is the most recent)
+                self.time_window['to'] = log_date_datetime
+            if not self.time_window['from']:
+                # if 'from' value is assigned, unix epoch beginning time is assigned
+                self.time_window['from'] = datetime.strptime('01 01 1970', '%d %m %Y')
+            validation_status = self.time_window['from'] <= log_date_datetime <= self.time_window['to']
+            if validation_status:
+                if not self.time_window['last']:
+                    # first date value which match the condition is assign as a 'first' date value for 'request_per_sec'
+                    self.time_window['last'] = log_date_datetime
+                # last date value which match the condition is assign as a 'last' value for 'request_per_sec'
+                self.time_window['first'] = log_date_datetime
+                return validation_status
+        except AttributeError:
+            # date not in the log entry (eg. first log row)
+            pass
 
     def look_for_responses(self, line: str) -> None:
         response_regex = re.compile(r'(?<=\s)\d{3}(?=\s)')
@@ -45,7 +147,8 @@ class Log:
             try:
                 self.output['responses'][response_value] += 1
             except KeyError:
-                self.output['responses'][response_value] = 0
+                # first time occurrence of response code
+                self.output['responses'][response_value] = 1
 
     def look_for_size(self, line: str) -> None:
         size_regex = re.compile(r'(?<=[2]\d{2}\s)\d*(?=\s)')
@@ -55,27 +158,34 @@ class Log:
             self.output['2xx_size'][0] += 1
             self.output['2xx_size'][1] += int(size_value)
 
-    def count_avr_2xx_size(self):
-        self.output['avg_2xx_size'] = self.output['2xx_size'][1] / self.output['2xx_size'][0]
+    def count_avg_2xx_size(self):
+        # number of 2xx codes by overall 2xx responses size division
+        try:
+            self.output['avg_2xx_size'] = self.output['2xx_size'][1] / self.output['2xx_size'][0]
+        except ZeroDivisionError:
+            self.output['avg_2xx_size'] = 0
 
+    def count_requests_per_second(self):
+        seconds = self.time_window['last'] - self.time_window['first']
+        req_per_sec = self.output['requests'] / seconds.total_seconds()
+        self.output['requests_per_seconds'] = f"{req_per_sec:.1f}"
 
-class Parser:
-    def __init__(self):
-        self.parser = argparse.ArgumentParser()
-        self.parser.add_argument('--fromm', '--from', required=False)
-        self.parser.add_argument('--to', required=False)
-        self.parser.add_argument('file')
-
-    def return_arguments(self):
-        args = self.parser.parse_args()
-        return args.fromm, args.to, args.file
+    def display_output(self):
+        requests = f"requests: {self.output['requests']}"
+        req_per_sec = f"requests/sec: {self.output['requests_per_seconds']}"
+        responses = f"responses: {self.output['responses']}"
+        avg = f"avg size of 2xx responses:" \
+              f" {self.output['avg_2xx_size']*10**(-6):.2f} Mb" \
+              f" ({round(self.output['avg_2xx_size'])} bytes)"
+        print("\n".join([requests, req_per_sec, responses, avg]))
 
 
 if __name__ == "__main__":
-    p = Parser()
-    fromm, to, file = p.return_arguments()
+    argument_parser = ArgumentParser()
+    argument_parser.check_arguments()
+    from_arg, to_arg, file = argument_parser.return_arguments()
 
-    l = Log(file, fromm, to)
-    l.open_file()
+    logg = Logg(file, from_arg, to_arg)
+    logg.open_file()
 
-    print(l.output)
+    logg.display_output()
